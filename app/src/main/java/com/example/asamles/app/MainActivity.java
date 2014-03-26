743a03e195d337d966afff63fd9b3f9563777ae2
gorbin <gorbin.e.o@gmail.com>
2014-03-26 21:05:41 +0700

    add bugtracker

--- app/src/main/java/com/example/asamles/app/MainActivity.java
+++ app/src/main/java/com/example/asamles/app/MainActivity.java
@@ -5,14 +5,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.bugsense.trace.BugSenseHandler;
+import com.bugsnag.android.Bugsnag;
 public class MainActivity extends BaseActivity implements FragmentManager.OnBackStackChangedListener {
     private ShareActionProvider mShareActionProvider;
     Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		BugSenseHandler.initAndStartSession(this, "27e64652");
+		Bugsnag.register(this, "5ef381ab46295ca342a9206abe44e869");
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainFragment())
