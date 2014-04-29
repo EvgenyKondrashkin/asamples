f2c4ebb737c3934d29fbde2653b18c84b6968584
gorbin <gorbin.e.o@gmail.com>
2014-04-29 21:37:40 +0700

    Cards styled

--- app/src/main/java/com/example/asamles/app/card/SocialCard.java
+++ app/src/main/java/com/example/asamles/app/card/SocialCard.java
@@ -2,6 +2,7 @@ package com.example.asamles.app.card;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,16 +15,38 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.asamles.app.R;
+import com.squareup.picasso.Picasso;
 
 public class SocialCard extends RelativeLayout {
-    public TextView name;
-	public TextView birthday;
-	public TextView contact;
-	public IconButton button;
-    public ImageView image;
+    private TextView name;
+	private TextView birthday;
+	private TextView contact;
+	public IconButton connect;
+	public IconButton share;
+    private ImageView image;
+	private ImageView divider;
+	private LinearLayout buttonLayout;
+	private int darkColor;
+	private int textColor;
+	private int color;
+    private int buttonTextColor;
+    private Context context;
 
 	public SocialCard(Context context, AttributeSet st) {
         super(context, st);
+        this.context = context;
+
+		TypedArray typedAttrs = context.obtainStyledAttributes(st,
+        R.styleable.social_card, 0, 0);
+		color = typedAttrs.getColor(R.styleable.social_card_color,
+        getResources().getColor(R.color.green));
+		textColor = typedAttrs.getColor(R.styleable.social_card_text_color,
+        getResources().getColor(R.color.green));
+		darkColor = typedAttrs.getColor(R.styleable.social_card_dark_color,
+        getResources().getColor(R.color.grey_light));
+		buttonTextColor = typedAttrs.getColor(R.styleable.social_card_button_text_color,
+        getResources().getColor(R.color.gallery_white));
+//		typedAttrs.recycle();
         init(context);
     }
 
@@ -33,10 +56,51 @@ public class SocialCard extends RelativeLayout {
 		name = (TextView) findViewById(R.id.name);
         birthday = (TextView) findViewById(R.id.birthday);
         contact = (TextView) findViewById(R.id.contact);
-		button = (IconButton) findViewById(R.id.connect);
+		connect = (IconButton) findViewById(R.id.connect);
+		share = (IconButton) findViewById(R.id.share);
         image = (ImageView) findViewById(R.id.image);
-
+		divider = (ImageView) findViewById(R.id.divider);
+		buttonLayout = (LinearLayout) findViewById(R.id.buttonLayout);
+		
+		name.setTextColor(textColor);
+		divider.setBackgroundColor(color);
+		buttonLayout.setBackgroundColor(darkColor);
+		connect.setBackgroundColor(color);
+		share.setBackgroundColor(color);
+		connect.setTextColor(buttonTextColor);
+		share.setTextColor(buttonTextColor);
+		image.setBackgroundColor(color);
 	}
-
+	
+	public void setName(String nameText){
+		name.setText(nameText);
+	}
+	
+	public void setBirthday(String birthdayText){
+		birthday.setText(birthdayText);
+	}
+	
+	public void setContact(String contactText){
+		contact.setText(contactText);
+	}
+	
+	public void setConnectButtonText(String connectText){
+		connect.setText(connectText);
+	}
+	
+	public void setShareButtonText(String shareText){
+		contact.setText(shareText);
+	}
+	
+	public void setImage(String uri, int placeholder, int error){
+		Picasso.with(context)
+            .load(uri)
+            .placeholder(placeholder)
+            .error(error)
+            .into(image);
+	}
+    public void setImageResource(int imageRes){
+        image.setImageResource(imageRes);
+    }
 
 }
--- app/src/main/java/com/example/asamles/app/socialnetwork/SocialNetworkMain.java
+++ app/src/main/java/com/example/asamles/app/socialnetwork/SocialNetworkMain.java
@@ -38,10 +38,10 @@ import java.util.List;
 
 public class SocialNetworkMain extends Fragment {
 
-    private Button fbButton, tButton, gButton, vkButton;
-    private TextView name;
-    boolean open;
-    SocialCard fbCard;
+    private SocialCard fbCard;
+	private SocialCard twCard;
+	private SocialCard gpCard;
+	private SocialCard vkCard;
 
     public static SocialNetworkMain newInstance() {
         return new SocialNetworkMain();
@@ -51,46 +51,11 @@ public class SocialNetworkMain extends Fragment {
     private class SessionStatusCallback implements Session.StatusCallback {
         @Override
         public void call(Session session, SessionState state, Exception exception) {
-            updateView();
+//            updateView();
             updateSocialCard(fbCard);
         }
     }
-    private void updateView() {
-        Session session = Session.getActiveSession();
-        if (session.isOpened()) {
-            open = true;
-            fbButton.setText("logout");
-            fbButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) { onClickLogout(); }
-            });
-            makeMeRequest(session);
-        } else {
-            open = false;
-            fbButton.setText("login");
-            fbButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) { onClickLogin(); }
-            });
-            defaultData();
-        }
-    }
-    private void onClickLogin() {
-        Session session = Session.getActiveSession();
-        List<String> permissions = new ArrayList<String>();
-        permissions.add("email");
-        permissions.add("user_birthday");
-        if (!session.isOpened() && !session.isClosed()) {
-            session.openForRead(new Session.OpenRequest(this).setPermissions(permissions).setCallback(statusCallback));
-        } else {
-            Session.openActiveSession(getActivity(), this, true, statusCallback);
-        }
-    }
 
-    private void onClickLogout() {
-        Session session = Session.getActiveSession();
-        if (!session.isClosed()) {
-            session.closeAndClearTokenInformation();
-        }
-    }
     public SocialNetworkMain() {
     }
 
@@ -100,21 +65,14 @@ public class SocialNetworkMain extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_social_network, container, false);
         fbCard = (SocialCard) rootView.findViewById(R.id.fb_card);
+		twCard = (SocialCard) rootView.findViewById(R.id.tw_card);
+		gpCard = (SocialCard) rootView.findViewById(R.id.gp_card);
+		vkCard = (SocialCard) rootView.findViewById(R.id.vk_card);
 
-
-
-        name = (TextView) rootView.findViewById(R.id.login);
-        fbButton = (Button) rootView.findViewById(R.id.facebook_button);
         Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
-//        fbButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View arg0) {
-//                fbButton.setTextColor(getActivity().getResources().getColor(R.color.green));
-//            }
-//        });
-        List<String> permissions = new ArrayList<String>();
-        permissions.add("email");
-        permissions.add("user_birthday");
+        // List<String> permissions = new ArrayList<String>();
+        // permissions.add("email");
+        // permissions.add("user_birthday");
         Session session = Session.getActiveSession();
         if (session == null) {
             if (savedInstanceState != null) {
@@ -125,102 +83,18 @@ public class SocialNetworkMain extends Fragment {
             }
             Session.setActiveSession(session);
             if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-                session.openForRead(new Session.OpenRequest(this).setPermissions(permissions).setCallback(statusCallback));
+                session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
             }
         }
         updateSocialCard(fbCard);
-        updateView();
-        TextView birth = (TextView)rootView.findViewById(R.id.birthday);
-        birth.setText("WAT");
-        tButton = (Button) rootView.findViewById(R.id.twitter_button);
-        gButton = (Button) rootView.findViewById(R.id.google_button);
-        vkButton = (Button) rootView.findViewById(R.id.vk_button);
-        tButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tButton.setTextColor(getActivity().getResources().getColor(R.color.green));
-                printHashKey();
-            }
-        });
-        gButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gButton.setTextColor(getActivity().getResources().getColor(R.color.green));
-                PublishToFeedInBackground();
-            }
-        });
-
-        vkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                vkButton.setTextColor(getActivity().getResources().getColor(R.color.green));
-            }
-		});
+//        updateView();
+        
+        printHashKey();
+       
         return rootView;
     }
-
-    private void updateSocialCard(SocialCard socialCard) {
-        Session session = Session.getActiveSession();
-        socialCard.button.setTextColor(Color.WHITE);
-        socialCard.button.setBackgroundColor(getResources().getColor(R.color.facebook));
-        if (session.isOpened()) {
-            open = true;
-
-            socialCard.button.setText("{fa-facebook}   Disconnect");
-            socialCard.button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    onClickLogout();
-                }
-            });
-            setDataFromMe(session, socialCard);
-            makeMeRequest(session);
-        } else {
-            open = false;
-            socialCard.button.setText("{fa-facebook} Connect");
-            socialCard.button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    onClickLogin();
-                }
-            });
-            defaultSocialCardData(socialCard);
-            defaultData();
-        }
-    }
-
-    private void defaultSocialCardData(SocialCard socialCard) {
-        socialCard.name.setText("NoName");
-        socialCard.birthday.setText("Birthday: unknown");
-        socialCard.contact.setText("id: unknown");
-        socialCard.image.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
-    }
-
-    private void setDataFromMe(final Session session, final SocialCard socialCard) {
-        Request request = Request.newMeRequest(session,
-                new Request.GraphUserCallback() {
-                    @Override
-                    public void onCompleted(GraphUser user, Response response) {
-                        if (session == Session.getActiveSession()) {
-                            if (user != null) {
-                                socialCard.name.setText(user.getName());
-                                socialCard.birthday.setText("Birthday: "+ user.getBirthday());
-                                socialCard.contact.setText("id: " + user.getId());
-                                Picasso.with(getActivity())
-                                        .load("https://graph.facebook.com/" + user.getId() + "/picture?type=large")
-                                        .placeholder(R.drawable.com_facebook_profile_picture_blank_square)
-                                        .error(R.drawable.error)
-                                        .into(socialCard.image);
-//                                socialCard.image.setImageBitmap(user.getId());
-                            }
-                        }
-                        if (response.getError() != null) {
-                            // Handle errors, will do so later.
-                        }
-                    }
-                });
-        request.executeAsync();
-    }
-
-    @Override
+	
+	@Override
     public void onStart() {
         super.onStart();
         Session.getActiveSession().addCallback(statusCallback);
@@ -244,8 +118,83 @@ public class SocialNetworkMain extends Fragment {
         Session session = Session.getActiveSession();
         Session.saveSession(session, outState);
     }
-    public void printHashKey() {
 
+    private void updateSocialCard(SocialCard socialCard) {
+        Session session = Session.getActiveSession();
+        if (session.isOpened()) {
+            socialCard.setConnectButtonText("{fa-facebook}   Disconnect");
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onClickLogout();
+                }
+            });
+            setDataFromMe(session, socialCard);
+//            makeMeRequest(session);
+        } else {
+//            open = false;
+            socialCard.setConnectButtonText("{fa-facebook} Connect");
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onClickLogin();
+                }
+            });
+            defaultSocialCardData(socialCard);
+//            defaultData();
+        }
+    }
+    private void onClickLogin() {
+        Session session = Session.getActiveSession();
+        List<String> permissions = new ArrayList<String>();
+        permissions.add("email");
+        permissions.add("user_birthday");
+        if (!session.isOpened() && !session.isClosed()) {
+            session.openForRead(new Session.OpenRequest(this).setPermissions(permissions).setCallback(statusCallback));
+        } else {
+            Session.openActiveSession(getActivity(), this, true, statusCallback);
+        }
+    }
+
+    private void onClickLogout() {
+        Session session = Session.getActiveSession();
+        if (!session.isClosed()) {
+            session.closeAndClearTokenInformation();
+        }
+    }
+    private void defaultSocialCardData(SocialCard socialCard) {
+        socialCard.setName("NoName");
+        socialCard.setBirthday("Birthday: unknown");
+        socialCard.setContact("id: unknown");
+        socialCard.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+    }
+
+    private void setDataFromMe(final Session session, final SocialCard socialCard) {
+        Request request = Request.newMeRequest(session,
+                new Request.GraphUserCallback() {
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        if (session == Session.getActiveSession()) {
+                            if (user != null) {
+                                socialCard.setName(user.getName());
+                                socialCard.setBirthday("Birthday: "+ user.getBirthday());
+                                socialCard.setContact("id: " + user.getId());
+//                                Picasso.with(getActivity())
+//                                        .load("https://graph.facebook.com/" + user.getId() + "/picture?type=large")
+//                                        .placeholder(R.drawable.com_facebook_profile_picture_blank_square)
+//                                        .error(R.drawable.error)
+//                                        .into(socialCard.image);
+                                socialCard.setImage("https://graph.facebook.com/" + user.getId() + "/picture?type=large", R.drawable.com_facebook_profile_picture_blank_square, R.drawable.error);
+//                                socialCard.image.setImageBitmap(user.getId());
+                            }
+                        }
+                        if (response.getError() != null) {
+                            // Handle errors, will do so later.
+                        }
+                    }
+                });
+        request.executeAsync();
+    }
+	
+    public void printHashKey() {
         try {
             PackageInfo info = getActivity().getPackageManager().getPackageInfo("com.example.asamles.app",
                     PackageManager.GET_SIGNATURES);
@@ -262,34 +211,7 @@ public class SocialNetworkMain extends Fragment {
         }
 
     }
-    private void defaultData(){
-        name.setText("NoName");
-    }
-    private void makeMeRequest(final Session session) {
-        // Make an API call to get user data and define a
-        // new callback to handle the response.
-        Request request = Request.newMeRequest(session,
-                new Request.GraphUserCallback() {
-                    @Override
-                    public void onCompleted(GraphUser user, Response response) {
-                        // If the response is successful
-                        if (session == Session.getActiveSession()) {
-                            if (user != null) {
-                                // Set the id for the ProfilePictureView
-                                // view that in turn displays the profile picture.
-//                                profilePictureView.setProfileId(user.getId());
-                                // Set the Textview's text to the user's name.
-//                                userNameView.setText(user.getName());
-                                name.setText(user.getName());
-                            }
-                        }
-                        if (response.getError() != null) {
-                            // Handle errors, will do so later.
-                        }
-                    }
-                });
-        request.executeAsync();
-    }
+
     public void PublishToFeedInBackground()
     {
         Session session = Session.getActiveSession();
