c8712b3ddd2d99a71932b4acf2d7f239e536fd38
gorbin <gorbin.e.o@gmail.com>
2014-05-08 21:10:41 +0700

    twitter + facebook in integration

--- app/src/main/java/com/example/asamles/app/socialintegration/SocialIntegrationMain.java
+++ app/src/main/java/com/example/asamles/app/socialintegration/SocialIntegrationMain.java
@@ -37,6 +37,14 @@ public class SocialIntegrationMain extends Fragment implements SocialNetworkMana
     public static final String SOCIAL_NETWORK_TAG = "SocialIntegrationMain";
     private SocialNetworkManager mSocialNetworkManager;
     private ADialogs progressDialog;
+//    private SocialNetworkID socialNetworkID;
+    private enum SocialNetworkID {
+        TWITTER,
+        LINKEDIN,
+        GOOGLEPLUS,
+        FACEBOOK;
+
+    }
 
     public static SocialIntegrationMain newInstance() {
         return new SocialIntegrationMain();
@@ -49,71 +57,147 @@ public class SocialIntegrationMain extends Fragment implements SocialNetworkMana
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        //===============================================================
-		mSocialNetworkManager = (SocialNetworkManager) getFragmentManager().findFragmentByTag(SOCIAL_NETWORK_TAG);
 
-		if (mSocialNetworkManager == null) {
-			mSocialNetworkManager = SocialNetworkManager.Builder.from(getActivity())
-					.twitter("BBQAUAVKYzmYtvEcNhUEvGiKd", "byZzHPxE1tkGmnPEj5zUyc7MG464Q1LgNRcwbBJV1Ap86575os")
-					.facebook()
-					.build();
-			getFragmentManager().beginTransaction().add((Fragment)mSocialNetworkManager, SOCIAL_NETWORK_TAG).commit();
-		}
-		mSocialNetworkManager.setOnInitializationCompleteListener(this);
-		//===============================================================
 		progressDialog = new ADialogs(getActivity());
         progressDialog.progress(false, "Fetching data...");
         View rootView = inflater.inflate(R.layout.fragment_social_network, container, false);
 
         fbCard = (SocialCard) rootView.findViewById(R.id.fb_card);
-        updateFacebookCard(fbCard);
-		fbCard.share.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
+//        updateFacebookCard(fbCard);
 
-                }
-            });
 
 		twCard = (SocialCard) rootView.findViewById(R.id.tw_card);
-		updateTwitterCard(twCard);
-		twCard.share.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    sendTweet();
-                }
-            });
+//		updateTwitterCard(twCard);
+
+
 		
 		gpCard = (SocialCard) rootView.findViewById(R.id.gp_card);
 		vkCard = (SocialCard) rootView.findViewById(R.id.vk_card);
-		
+
+        //===============================================================
+        mSocialNetworkManager = (SocialNetworkManager) getFragmentManager().findFragmentByTag(SOCIAL_NETWORK_TAG);
+
+        if (mSocialNetworkManager == null) {
+            mSocialNetworkManager = SocialNetworkManager.Builder.from(getActivity())
+                    .twitter("BBQAUAVKYzmYtvEcNhUEvGiKd", "byZzHPxE1tkGmnPEj5zUyc7MG464Q1LgNRcwbBJV1Ap86575os")
+                    .facebook()
+                    .build();
+            getFragmentManager().beginTransaction().add((Fragment)mSocialNetworkManager, SOCIAL_NETWORK_TAG).commit();
+        }
+        mSocialNetworkManager.setOnInitializationCompleteListener(this);
+        //===============================================================
         return rootView;
     }
-    private void defaultSocialCardData(SocialCard socialCard) {
+    private void defaultSocialCardData(SocialCard socialCard, SocialNetworkID enumId) {
         socialCard.setName("NoName");
         socialCard.setBirthday("Birthday: unknown");
         socialCard.setContact("id: unknown");
+//        SocialNetworkID enumId = SocialNetworkID.values()[id];
+        switch (enumId){
+            case TWITTER:
+                socialCard.setImageResource(R.drawable.twitter_user);
+                break;
+            case LINKEDIN:
+                break;
+            case GOOGLEPLUS:
+                break;
+            case FACEBOOK:
+                socialCard.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+                break;
+        }
     }
-
+// ================Facebook=========================================================================
     private void updateFacebookCard(final SocialCard socialCard) {
 		socialCard.setShareButtonText("{fa-share}  Share");
-
+        final SocialNetwork fb = mSocialNetworkManager.getFacebookSocialNetwork();
+        if(fb.isConnected()) {
             socialCard.setConnectButtonText("{fa-facebook}   Disconnect");
-
-//			setFacebookCardFromUser();
-
-
-            defaultSocialCardData(socialCard);
-
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fb.logout();
+                    updateFacebookCard(socialCard);
+                }
+            });
+            socialCard.share.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    fb.requestPostMessage("Hello from ASample!");
+                }
+            });
+            fb.requestPerson();
+        } else {
+            socialCard.setConnectButtonText("{fa-facebook}   Connect");
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fb.requestLogin();
+                }
+            });
+            socialCard.share.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), "You should connect first!",Toast.LENGTH_LONG).show();
+                }
+            });
+            defaultSocialCardData(socialCard, SocialNetworkID.FACEBOOK);
+        }
     }
 
-	public void setFacebookCardFromUser(SocialPerson socialPerson){
-
+	public void setFacebookCardFromUser(SocialPerson socialPerson, SocialCard socialCard){
+        socialCard.setName(socialPerson.name);
+        socialCard.setBirthday("Birthday: I can't!");
+        socialCard.setContact("ID: " + socialPerson.id);
+        socialCard.setImage("https://graph.facebook.com/" + socialPerson.id + "/picture?type=large", R.drawable.com_facebook_profile_picture_blank_square, R.drawable.error);
     }
-//==============================================================================================
+// ================Twitter==========================================================================
+    private void updateTwitterCard(final SocialCard socialCard) {
+        socialCard.setShareButtonText("{fa-share}  Share tweet");
+        final SocialNetwork tw = mSocialNetworkManager.getTwitterSocialNetwork();
+        if(tw.isConnected()){
+            socialCard.setConnectButtonText("{fa-twitter}   Logout");
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    tw.logout();
+                    updateTwitterCard(socialCard);
+                }
+            });
+            socialCard.share.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    tw.requestPostMessage("Hello from ASample");
+                }
+            });
+            tw.requestPerson();
+        } else {
+            socialCard.setConnectButtonText("{fa-twitter} login");
+            socialCard.connect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    tw.requestLogin();
+                }
+            });
+            socialCard.share.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), "You should login first!",Toast.LENGTH_LONG).show();
+                }
+            });
+            defaultSocialCardData(socialCard, SocialNetworkID.TWITTER);
+        }
+    }
+    public void setTwitterCardFromUser(SocialPerson socialPerson, SocialCard socialCard){
+        socialCard.setName(socialPerson.name);
+        socialCard.setBirthday("Birthday: I can't!");
+        socialCard.setContact("ID: " + socialPerson.id);
+        socialCard.setImage(socialPerson.avatarURL, R.drawable.twitter_user, R.drawable.error);
+    }
+
+
+//==================================================================================================
 	@Override
     public void onSocialNetworkManagerInitialized() {
         for (SocialNetwork socialNetwork : mSocialNetworkManager.getInitializedSocialNetworks()) {
             socialNetwork.setOnLoginCompleteListener(this);
             socialNetwork.setOnRequestSocialPersonListener(this);
         }
+        updateFacebookCard(fbCard);
+        updateTwitterCard(twCard);
     }
 
     @Override
@@ -126,45 +210,16 @@ public class SocialIntegrationMain extends Fragment implements SocialNetworkMana
             socialNetwork.setOnRequestSocialPersonListener(null);
         }
     }
-//=================================================================================================
-	public boolean isOnline() {
-        return false;
-	}
-	private void updateTwitterCard(SocialCard socialCard) {
-		socialCard.setShareButtonText("{fa-share}  Share tweet");
-		if(isOnline()){
-			socialCard.setConnectButtonText("{fa-twitter}   Logout");
-            socialCard.connect.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-
-                    Toast.makeText(getActivity(), "Signed Off", Toast.LENGTH_SHORT).show();
-                }
-            });
-//			setTwitterCardFromUser();
-		} else {
-			socialCard.setConnectButtonText("{fa-twitter} login");
-            socialCard.connect.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    mSocialNetworkManager.getTwitterSocialNetwork().requestLogin();
-                    Toast.makeText(getActivity(), "Signed in", Toast.LENGTH_SHORT).show();
-                }
-            });
-            defaultSocialCardData(socialCard);
-		}
-	}
-	public void setTwitterCardFromUser(SocialPerson socialPerson){
-
-    }
-
-
-	public void sendTweet() {
-		String message = "Test tweet message from ASample";
-
-	}
-
     @Override
     public void onLoginSuccess(int id) {
-        mSocialNetworkManager.getSocialNetwork(id).requestPerson();
+        switch (id){
+            case 1:
+                updateTwitterCard(twCard);
+                break;
+            case 4:
+                updateFacebookCard(fbCard);
+                break;
+        }
     }
 
     @Override
@@ -173,25 +228,25 @@ public class SocialIntegrationMain extends Fragment implements SocialNetworkMana
         handleError(id, errorReason);
     }
     private void handleError(int id, String errorReason) {
-        Toast.makeText(getActivity(), "ERROR: " + errorReason, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "ERROR: " + errorReason, Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onRequestSocialPersonSuccess(int i, SocialPerson socialPerson) {
-
+    public void onRequestSocialPersonSuccess(int id, SocialPerson socialPerson) {
+        switch (id){
+            case 1:
+                setTwitterCardFromUser(socialPerson, twCard);
+                break;
+            case 4:
+                setFacebookCardFromUser(socialPerson, fbCard);
+                break;
+        }
     }
 
     @Override
     public void onRequestSocialPersonFailed(int i, String s) {
+        Toast.makeText(getActivity(), "Request user ERROR: " + s, Toast.LENGTH_LONG).show();
+    }
+//==================================================================================================
 
-    }
-    private static enum UserState {
-        LOGIN,
-        LOGOUT
-    }
-    UserState userState;
-    private void switchUIState(UserState state, int id) {
-        userState = state;
-
-    }
 }
